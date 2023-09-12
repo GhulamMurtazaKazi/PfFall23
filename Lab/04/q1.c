2b7ddda35508982cf0c7d9ab0069fa2112823159
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-09-12 07:59:07 -0700

    Create q1.c

---
+++ Lab/04/q1.c
@@ -0,0 +1,29 @@
+/*
+*Programmer:Ghulam Murtza
+*Date:12-9-23
+*Description:This program prints a pyramid of stars for n numbers.*/
+
+
+//--included files--//
+#include <stdio.h>
+
+int main() {
+	int n, i, j; // row count for '*', counter for rows, counter for '*'
+	i = 0, j = 0; // initialize i and j
+
+	printf("Input any number 'n': ");
+	scanf("%d", &n);
+
+	while (i < n){
+		
+		// adding '*'
+		j = 0;
+		while (j <= i) {
+			printf("*");
+			j++;
+		} 		
+		printf("\n"); // starting new line
+		i++;
+	}
+
+}// end main
