53fe4ba5094c3144002459d526788b260019650c
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-09-19 08:56:09 +0500

    Create Q1.c

---
+++ Lab/05/Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+int number;
+printf("enter a number\n");
+scanf("%d",&number);
+if (number/3==0)
+{ printf("the number is multiple of 3");
+} else {
+printf("the number is not multiple of 3");
+}
+return 0;
+}
