b4726822b1489e285401cf0fe244ec4f895c8020
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-09-30 03:56:36 -0700

    Create Q2.c

---
+++ Lab/03/Q2.c
@@ -0,0 +1,12 @@
+QUESTION#2
+Ahmed’s ball container is 20 centimeters tall, 10 centimeters long and 10 centimeters wide. Find the volume of container in C.
+#include<stdio.h>
+int main () {
+	int height=20,length=10,width=10;
+	printf("height is 20 cm\n");
+		printf("length is 10 cm\n");
+			printf("width is 10 cm\n");
+				printf("volume=length*width*height\n");
+					printf("the volume of ahmed's ball container is %d cm",length*width*height);
+					return 0;
+}
