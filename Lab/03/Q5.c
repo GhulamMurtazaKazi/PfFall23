18dd3569d08c4210b8cf6d495830100a8ca19bb5
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-09-30 05:06:41 -0700

    Create Q5.c

---
+++ Lab/03/Q5.c
@@ -0,0 +1,2 @@
+QUESTION#5
+A bus leaves the university to take students on a field trip. The bus travels 10 kilometers south, 10 kilometers west, another 5 kilometers south and 15 kilometers north with the fuel consumption of 2 liters/km.Calculate how many kilometers it has covered and how much fuel it has consumed on a field trip?
