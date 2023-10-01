ce276644c7e5e9415793c83317abf8596e975b93
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-01 00:58:15 -0700

    Update Q9.c

--- Lab/05/Q9.c
+++ Lab/05/Q9.c
@@ -2,31 +2,32 @@ Write a C program to find all roots of a quadratic equation, it is required to t
 discriminant=b ×b-(4×a×c)
 Compute roots based on the nature of discriminant.
   
+#include<stdio.h>
 #include<math.h>
 int main () {
-	float a,b,c,d;
-	printf("enter a,b,c\n");
-	scanf("%f %f %f",&a,&b,&c);
-	d=(b*b)-(4*a*c);
-	if (a==0) {
-		printf("invalid a can't be equal to 0");
-	} else {
-		if  (d==0) {
-			printf("roots are equal");
-			printf("%f",-b/(2*a));
-		} else if (d>0) {
-				d=sqrt(d);
-				printf("root 1:%f\n",(-b+d)/(2*a));
-					printf("root 2:%f\n",(-b-d)/(2*a));
-			
-		} else {
-			d=fabs(d);
-			d=sqrt(d);
-			printf("root 1:%f+%fi\n",(-b/2*a),(d/2*a));
-				printf("root 2:%f-%fi\n",(-b/2*a),(d/2*a));
-			
-		}
-	}
-	
-	return 0;
+    float a,b,c,d;
+    printf("enter a,b,c\n");
+    scanf("%f %f %f ",&a,&b,&c);
+    d=(b*b)-(4*a*c);
+    if (a==0) {
+        printf("invalid a can't be equal to 0");
+    } else {
+        if  (d==0) {
+            printf("roots are equal");
+            printf("%f",-b/(2*a));
+        } else if (d>0) {
+                d=sqrt(d);
+                printf("root 1:%f",(-b+d)/(2*a));
+                printf("root 2:%f",(-b-d)/(2*a));
+            
+        } else {
+            d=fabs(d);
+            d=sqrt(d);
+            printf("root 1:%f+%fi",(-b/(2*a)),(d/(2*a)));
+            printf("root 2:%f-%fi",(-b/(2*a)),(d/(2*a)));
+            
+        }
+    }
+    
+    return 0;
 }
