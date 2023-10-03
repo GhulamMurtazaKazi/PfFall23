27792470906ea6434817867e6185f9ade7ff5698
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-03 10:36:57 +0500

    Create Q4.c

---
+++ Lab/06/Q4.c
@@ -0,0 +1,7 @@
+Consider Two integers a and b taken as input from the user. Using Loops iterate the value of
+a till the value of b.
+If the value of a<=9 the output should correspond to the English representation of the
+numbers i.e., 8=Eight, 9=Nine etc.
+If the iteration exceeds 9 then the programs should print if the exceeded number is even or
+odd.
+  
