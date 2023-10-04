68da042bd796f5236049336650220a4fcc2e3375
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-04 12:42:44 +0500

    Update Q6.c

--- Lab/06/Q6.c
+++ Lab/06/Q6.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main () {
 	int user,computer,match_sticks=21;
-	while (match_sticks>0) {
-		printf("choose any nmumber of match sticks between 1 to 4\n");
+	while (match_sticks>1) {
+		printf("choose any number of match sticks between 1 to 4\n");
 		scanf("%d",&user);
 		computer=5-user;
 		printf("computer picks %d\n",computer);
 		match_sticks=match_sticks-(computer+user);
+		printf("remaining match sticks are %d\n",match_sticks);
 		
 	} 
-	printf("you chosse the last remaining 1 match stick so you loss");
+	printf("you choose the last remaining 1 match stick so you loss");
 	return 0;
 }
