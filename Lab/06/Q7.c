7045b7afea2daad0b6b5846ffb07b2950443a95c
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-04 13:24:45 -0700

    Update Q7.c

--- Lab/06/Q7.c
+++ Lab/06/Q7.c
@@ -1,22 +1,22 @@
 Write a C Program that takes a user input array and prints the sum of its elements.
 Input: {1,2,3,4,5,6,7,8,9}
 Output: 45
-  #include<stdio.h>
+#include<stdio.h>
+
 int main () {
-	int n,i=0,sum=0;
-	printf("enter the length of array\n");
-	scanf("%d",&n);
-	float list[n];
-	printf("enter numbers\n");
-	
-	while (i<n) {
-	scanf("%f",&list[i]);
-	sum=sum+list[i];
-	++i;
-}
-printf("%d",sum);
-return 0;
-	
-	
-	
+    float sum = 0, numbers[10000];
+    int i, size;
+
+    printf("Enter the size of array:");
+    scanf("%d", &size);
+
+    for(i = 0; i < size; i++) {
+        printf("Enter number %d: ", i+1);
+        scanf("%f", &numbers[i]);
+        sum += numbers[i];
+    }
+
+    printf("The sum of the numbers is %f", sum);
+
+    return 0;
 }
