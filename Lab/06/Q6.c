4765887b4a2836753e18d4d693417f8951178ac5
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-04 12:43:39 +0500

    Update Q6.c

--- Lab/06/Q6.c
+++ Lab/06/Q6.c
@@ -1,3 +1,12 @@
+Write a program for a match-stick game between the computer and a user. Your 
+program should ensure that the computer always wins. Rules for the game are as 
+follows:
+1. There are 21 matchsticks.
+2. The computer asks the player to pick 1,2,3 or 4 match sticks.
+3. After the person picks, the computer does its picking.
+4. Whoever is forced to pick up the last matchstick loses the game.
+
+	
 #include<stdio.h>
 int main () {
 	int user,computer,match_sticks=21;
