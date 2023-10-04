69b7865048bb86fb054f901d06032a6136a9bac0
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-04 13:22:49 -0700

    Update Q5.c

--- Lab/06/Q5.c
+++ Lab/06/Q5.c
@@ -15,3 +15,51 @@ Write a C program that produces the following output:
 6 6 6 6
 
 Note: Only use single loops (No Nested Loops)
+#include<stdio.h>
+int main () {
+	int i=1;
+while (i<=4) {
+	printf("0\t");
+	++i;
+} 
+printf("\n");
+i=1;
+while (i<=2) {
+	printf("\t1");
+	++i;
+} 
+i=1;
+printf("\n");
+while (i<=4) {
+	printf("2\t");
+	++i;
+	
+}
+printf("\n");
+i=1;
+while (i<=2) {
+	printf("\t3");
+	++i;
+}
+i=1;
+printf("\n");
+while (i<=4) {
+	printf("4\t");
+	++i;
+	
+} 
+printf("\n");
+i=1;
+while (i<=2) {
+	printf("\t5");
+	++i;
+}
+i=1;
+printf("\n");
+while (i<=4) {
+	printf("6\t");
+	++i;
+	
+} 
+	return 0;
+}
