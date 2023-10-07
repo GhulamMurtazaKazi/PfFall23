22315f820e2dddeaff56323a61a24b30b1dadfbf
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-07 09:03:44 -0700

    Update Q1.c

--- Lab/06/Q1.c
+++ Lab/06/Q1.c
@@ -1,22 +1,22 @@
 Write a C Program that takes any number from the user and identifies if the number is a
 perfect number or not.
-  #include<stdio.h>
+#include<stdio.h>
 int main () {
-	int i=1,number;
-	printf("enter a number\n");
+	int number,i=1,sum=0;
+	printf("enter the number\n");
 	scanf("%d",&number);
-	while (i<=number) {
-	if 	(number%i==0) {
-		if (i*i==number) {
-			printf("this is a perfect square");
+	while (i<number) {
+		if (number%i==0) {
+			sum=sum+i;
+		}
+		++i;
+
+	}
+	if (sum==number) {
+		printf("%d is a perfect number",number);
+		} else {
+			printf("%d isnot a perfect number",number);
+			
+		}
 		return 0;
-	}
-		
-	}
-	++i;
-		
-	}
-	printf("not a perfect square");
-	
-	return 0;
-}
+} 
