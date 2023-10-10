a4f4d338c50e9d7fed8b88540a2bcf09ed37e9b3
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-09 22:28:30 -0700

    Create Q10.c

---
+++ Lab/07/Q10.c
@@ -0,0 +1,9 @@
+10. Given an unsorted array A of size N that contains only non-negative integers, find a continuous sub-
+array which adds to a given number S. In case of multiple subarrays, return the subarray which comes
+
+first on moving from left to right.
+Input:
+Enter Number S = 12
+Array: {1, 2, 3, 7, 5}
+Output:
+The Elements from Index 1 to 3 when summed results in the output of 12.
