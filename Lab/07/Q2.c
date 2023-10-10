cc8045e03de4940c6e3c5345bedf8425655b7644
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-09 22:29:35 -0700

    Create Q2.c

---
+++ Lab/07/Q2.c
@@ -0,0 +1,5 @@
+2. Given an array of integers of size N and an integer “d”, the task is to rotate the array elements to the
+left by d positions. Note: Solve the question without declaring another array. (The input array itself
+must be modified).
+Input: {1, 2, 3, 4, 5, 6, 7}, d = 2
+Output: {3, 4, 5, 6, 7, 1, 2}
