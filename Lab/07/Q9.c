2b709aed4eef07f0d19f2b40c0742cd22dd9579d
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-09 22:28:00 -0700

    Create Q9.c

---
+++ Lab/07/Q9.c
@@ -0,0 +1,8 @@
+9. Junaid wants to keep track of all mobile phone bills in his X company branches. Let Y be the number
+of company mobile phones in each branch. Create a 2D array for bill amount, where keep track of
+branch ID in row subscript, mobile phone IDs in column subscript. Ask users to enter a bill for all mobile
+phones in all branches. Your program should print the following:
+- Total bill for all branches
+- Total bill for each branch
+- Branch ID where maximum bill arrived
+- Branch and Mobile Phone IDs where bill is highest of all mobile phones.
