e0777eed0537fca522f4eff2eb195d9d53c612bf
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-24 09:10:53 +0500

    Create 6.c

---
+++ Lab/08/6.c
@@ -0,0 +1,15 @@
+QUESTION # 6
+You are given an array of integers. Write a C program that
+defines a user-defined function processArray to calculate the sum, maximum, and
+minimum values in the array. The program should take the array and its size as
+parameters and use the function to compute these values.
+
+Input:Array is [4, 8, 1, 15, 6]
+Output:
+Array Sum:
+34
+Maximum
+Value: 15
+Minimum Value: 1
+
+  
