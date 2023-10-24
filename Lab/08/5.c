aa803fa15c0ce7ef2adb9ddb9a01fdee7880c108
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-24 09:08:26 +0500

    Create 5.c

---
+++ Lab/08/5.c
@@ -0,0 +1,9 @@
+QUESTION # 5
+In this C program, you are tasked with creating a function
+called decideCarUsage that helps users decide whether they should use their car
+on a particular day of the week. Users provide the numeric part of their car's
+number and the day of the week (1 to 7). The program applies a simple rule:
+even-numbered cars should be used on even days, and odd-numbered cars on odd
+days. The function returns 1 if the car should be used and 0 if it should not.
+
+
