689414f70091d277b3e87fd31ce83dc8fd2eb664
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-31 08:54:26 +0500

    Update 1.c

--- Lab/pointers/1.c
+++ Lab/pointers/1.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+void swap(int*a,int*b) {
+	int temp=*b;
+	*b=*a;
+	*a=temp;
+}
+
 int main () {
 	int j=2,k=5;
 	printf("j=%d,k=%d\n",j,k);
@@ -7,8 +13,3 @@ int main () {
 		return 0;
 }
 
-void swap(int*a,int*b) {
-	int temp=*b;
-	*b=*a;
-	*a=temp;
-}
