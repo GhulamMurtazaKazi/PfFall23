a55c775d9fe9ba9e8885d064a5bdfc07402688a2
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-31 09:21:51 +0500

    Create 3.c

---
+++ Lab/pointers/3.c
@@ -0,0 +1,6 @@
+Write a C program that makes 3 arrays of size N (N is to be provided by the user, use variable
+length arrays) of type char, int, long long int. Initialize them with random values. Print the
+addresses and values stored in all these arrays one by one using pointer arithmetic. Explain in
+comments why adding to pointers of different types works differently.
+
+  
