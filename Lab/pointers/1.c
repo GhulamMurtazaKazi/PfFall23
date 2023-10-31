c199a799aa3ee22d3b605a933bc2be295cf57701
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-10-31 09:02:28 +0500

    Update 1.c

--- Lab/pointers/1.c
+++ Lab/pointers/1.c
@@ -4,7 +4,8 @@ void swap(int*a,int*b) {
 	*b=*a;
 	*a=temp;
 }
-
+/*we were not using pointers initially that's why the program was not producing the desired result.The copy of argument is passed to the 
+function that's why any change in parameter doesnot affect argument*/ 	
 int main () {
 	int j=2,k=5;
 	printf("j=%d,k=%d\n",j,k);
