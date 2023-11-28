6b0d184bd8b817bc45be736e584995445de4434d
GhulamMurtazaKazi <142872097+GhulamMurtazaKazi@users.noreply.github.com>
2023-11-28 09:26:02 +0500

    Create 1.c

---
+++ Lab/12/1.c
@@ -0,0 +1,1 @@
+1. Read any CSV file and print total number of rows and columns before displaying its data in a tabular form using , \t and '|'. Assume that first line is the header and when displaying it, it must be followed by a blank line before displaying the data.
